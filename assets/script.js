d1eadee6c22cc7d3a416c22b908010b98dd045cd
Ricardo Garcia <ricardougarcia@gmail.com>
2021-10-26 21:20:55 -0400

    added css styling

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES-----------------
+
+// FUNCTIONS--------------------
+// grab info about repos
+function getApi()
+    // var requestURL here = "link##":
+
+// fetch method next: 
+fetch(requestUrl)
+.then(function(response) {
+    return response.json();
+})    
+.then(function (data) {
+    console.log(data)
+    //Loop over the data to generate a table, each table row will have a link to the repositiory
+})
+// 
+// DESIGN-----------------------
+
+// USER INTERACTIONS------------
